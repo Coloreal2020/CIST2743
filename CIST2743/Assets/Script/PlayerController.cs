66e3111b20c7c8302ea4881cb6f2e3096c990f36
Coloreal2020 <70281772+Coloreal2020@users.noreply.github.com>
2020-11-18 20:32:59 -0500

    game pt2

--- CIST2743/Assets/Script/PlayerController.cs
+++ CIST2743/Assets/Script/PlayerController.cs
@@ -7,20 +7,28 @@ public class PlayerController: MonoBehaviour
 {
     private float movementInputDirection;
 
+    private int amountOfJumpsLeft;
+
     private bool isFacingRight = true;
     private bool isWalking;
     private bool isGrounded;
+    private bool isTouchingWall;
+    private bool isWallSliding;
     private bool canJump;
 
     private Rigidbody2D rb;
     private Animator anim;
 
+    public int amountOfJumps = 1;
+
     public float movementSpeed = 10.0f;
     public float jumpForce = 16.0f;
     public float GroundCheckRadius;
-
+    public float wallCheckDistance;
+    public float wallSlidSpeed;
 
     public Transform GroundCheck;
+    public Transform wallCheck;
 
     public LayerMask whatIsGround;
 
@@ -29,6 +37,7 @@ public class PlayerController: MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        amountOfJumpsLeft = amountOfJumps;
     }
 
     // Update is called once per frame
@@ -38,6 +47,7 @@ public class PlayerController: MonoBehaviour
         ChechMovementDirection();
         UpdateAnimations();
         CheckIfCanJump();
+        CheckIfWallSliding();
     }
 
     private void FixedUpdate()
@@ -46,20 +56,39 @@ public class PlayerController: MonoBehaviour
         CheckSurroundings();
     }
 
+    private void CheckIfWallSliding()
+    {
+        if(isTouchingWall && !isGrounded && rb.velocity.y < 0)
+        {
+            isWallSliding = true;
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
     private void CheckSurroundings()
     {
         isGrounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, whatIsGround);
+
+        isTouchingWall = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
     }
 
     private void CheckIfCanJump()
     {
         if(isGrounded && rb.velocity.y <= 0)
         {
-            canJump = true;
+            amountOfJumpsLeft = amountOfJumps;
+        }
+        
+        if(amountOfJumpsLeft <= 0)
+        {
+            canJump = false;
         }
         else
         {
-            canJump = false;
+            canJump = true;
         }
     }
     private void ChechMovementDirection()
@@ -95,27 +124,49 @@ public class PlayerController: MonoBehaviour
     private void UpdateAnimations()
     {
         anim.SetBool("isWalking", isWalking);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.velocity.y);
+        anim.SetBool("isWallSliding", isWallSliding);
     }
     private void jump()
     {
         if(canJump)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            amountOfJumpsLeft--;
         }
     }
     private void ApplyMovement()
     {
-        rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+        }
+
+        if (isWallSliding)
+        {
+            if(rb.velocity.y < -wallSlidSpeed)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -wallSlidSpeed);
+            }
+        }
     }
 
     private void Flip()
     {
-        isFacingRight = !isFacingRight;
-        transform.Rotate(0.0f, 180.0f, 0.0f);
+        if (!isWallSliding)
+        {
+            isFacingRight = !isFacingRight;
+            transform.Rotate(0.0f, 180.0f, 0.0f);
+        }
+      
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(GroundCheck.position, GroundCheckRadius);
+
+        Gizmos.DrawLine(wallCheck.position,new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y, wallCheck.position.z));
     }
 }
+ 
