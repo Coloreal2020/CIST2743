e414328bad0aba162978f33ec7847d8f771cfe21
Coloreal2020 <70281772+Coloreal2020@users.noreply.github.com>
2020-12-04 18:03:53 -0500

    LastDay

--- CIST2743/Assets/Script/PlayerController.cs
+++ CIST2743/Assets/Script/PlayerController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class PlayerController: MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     private float movementInputDirection;
     private float TurnTimer;
-
+    private float dashTimeLeft;
+    private float lastImageXpos;
+    private float lastDsah = -100;
 
     private int amountOfJumpsLeft;
     private int facingDirection = 1;
@@ -20,9 +22,10 @@ public class PlayerController: MonoBehaviour
     private bool canJump;
     private bool isTouchingLedge;
     private bool canClimbLege = false;
-    private bool ledgeDetected;
+    private bool ledgeDectected;
     private bool canMove;
     private bool canFlip;
+    private bool isDashing;
 
     private Vector2 ledgePosBot;
     private Vector2 ledgePos1;
@@ -44,6 +47,10 @@ public class PlayerController: MonoBehaviour
     public float wallHopForce;
     public float wallJumpForce;
     public float turnTimerSet = 0.1f;
+    public float dashTime;
+    public float dashSpeed;
+    public float distanceBetweenImages;
+    public float dashCoolDown;
 
     public float ledgeClimbXOffset1 = 0f;
     public float ledgeClimbYOffset1 = 0f;
@@ -57,6 +64,7 @@ public class PlayerController: MonoBehaviour
     public Transform GroundCheck;
     public Transform wallCheck;
     public Transform ledgeCheck;
+
 
     public LayerMask whatIsGround;
 
@@ -79,6 +87,8 @@ public class PlayerController: MonoBehaviour
         CheckIfCanJump();
         CheckIfWallSliding();
         CheckLedgeClimb();
+        CheckDash();
+
     }
 
     private void FixedUpdate()
@@ -89,7 +99,7 @@ public class PlayerController: MonoBehaviour
 
     private void CheckIfWallSliding()
     {
-        if(isTouchingWall && !isGrounded && rb.velocity.y < 0 && !canClimbLege)
+        if (isTouchingWall && !isGrounded && rb.velocity.y < 0 && !canClimbLege)
         {
             isWallSliding = true;
         }
@@ -99,43 +109,44 @@ public class PlayerController: MonoBehaviour
         }
     }
 
+
     private void CheckLedgeClimb()
     {
-        if(ledgeDetected && !canClimbLege)
+        if (ledgeDectected && !canClimbLege)
         {
             canClimbLege = true;
 
             if (isFacingRight)
             {
                 ledgePos1 = new Vector2(Mathf.Floor(ledgePosBot.x + wallCheckDistance) - ledgeClimbXOffset1, Mathf.Floor(ledgePosBot.y) + ledgeClimbYOffset1);
-                ledgePos2 = new Vector2(Mathf.Floor(ledgePosBot.x + wallCheckDistance) - ledgeClimbXOffset2, Mathf.Floor(ledgePosBot.y) + ledgeClimbYOffset2);
+                ledgePos2 = new Vector2(Mathf.Floor(ledgePosBot.x + wallCheckDistance) + ledgeClimbXOffset2, Mathf.Floor(ledgePosBot.y) - ledgeClimbYOffset2);
             }
             else
             {
                 ledgePos1 = new Vector2(Mathf.Ceil(ledgePosBot.x - wallCheckDistance) + ledgeClimbXOffset1, Mathf.Floor(ledgePosBot.y) + ledgeClimbYOffset1);
                 ledgePos2 = new Vector2(Mathf.Ceil(ledgePosBot.x - wallCheckDistance) - ledgeClimbXOffset2, Mathf.Floor(ledgePosBot.y) + ledgeClimbYOffset2);
-
             }
+
             canMove = false;
             canFlip = false;
-            
-        }
-        if (canClimbLege)
-        {
-            transform.position = ledgePos1;
+
+            if (canClimbLege)
+            {
+                transform.position = ledgePos1;
+            }
+            anim.SetBool("canClimbLege", canClimbLege);
         }
     }
 
-    public void FinishLedgeClimb()
+    public void FinsihLedgeClimb()
     {
         canClimbLege = false;
         transform.position = ledgePos2;
         canMove = true;
         canFlip = true;
-        ledgeDetected = false;
-        
+        ledgeDectected = false;
+        anim.SetBool("canClimbLege", canClimbLege);
     }
-
 
     private void CheckSurroundings()
     {
@@ -144,21 +155,22 @@ public class PlayerController: MonoBehaviour
         isTouchingWall = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
         isTouchingLedge = Physics2D.Raycast(ledgeCheck.position, transform.right, wallCheckDistance, whatIsGround);
 
-        if(isTouchingWall && !isTouchingLedge && !ledgeDetected)
+        if (isTouchingWall && !isTouchingLedge && !ledgeDectected)
         {
-            ledgeDetected = true;
+            ledgeDectected = true;
             ledgePosBot = wallCheck.position;
         }
+
     }
 
     private void CheckIfCanJump()
     {
-        if((isGrounded && rb.velocity.y <= 0) || isWallSliding)
+        if ((isGrounded && rb.velocity.y <= 0) || isWallSliding)
         {
             amountOfJumpsLeft = amountOfJumps;
         }
-        
-        if(amountOfJumpsLeft <= 0)
+
+        if (amountOfJumpsLeft <= 0)
         {
             canJump = false;
         }
@@ -178,7 +190,7 @@ public class PlayerController: MonoBehaviour
             Flip();
         }
 
-        if(rb.velocity.x != 0)
+        if (rb.velocity.x != 0)
         {
             isWalking = true;
         }
@@ -199,17 +211,73 @@ public class PlayerController: MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * variableJumpHeightMultiplier);
         }
-      
+        if (Input.GetButtonDown("Horizontal") && isTouchingWall)
+        {
+            canMove = false;
+            canFlip = false;
+
+            TurnTimer = turnTimerSet;
+        }
+        if (!canMove)
+        {
+            TurnTimer -= Time.deltaTime;
+            if (TurnTimer <= 0)
+            {
+                canMove = true;
+                canFlip = true;
+            }
+        }
+        if (Input.GetButtonDown("Dash"))
+        {
+            if (Time.time >= (lastDsah + dashCoolDown))
+                AttemptToDash();
+        }
     }
 
+    private void AttemptToDash()
+    {
+        isDashing = true;
+        dashTimeLeft = dashTime;
+        lastDsah = Time.time;
 
+        PlayerAfterImagePool.Instance.GetFromPool();
+        lastImageXpos = transform.position.x;
+
+    }
+
+    private void CheckDash()
+    {
+        if (isDashing)
+        {
+            if(dashTimeLeft > 0)
+            {
+                canMove = false;
+                canFlip = false;
+                rb.velocity = new Vector2(dashSpeed * facingDirection, rb.velocity.y);
+                dashTimeLeft -= Time.deltaTime;
+
+                if (Mathf.Abs(transform.position.x - lastImageXpos) > distanceBetweenImages)
+                {
+                    PlayerAfterImagePool.Instance.GetFromPool();
+                    lastImageXpos = transform.position.x;
+                }
+            }
+            if(dashTimeLeft <= 0 || isTouchingWall)
+            {
+                isDashing = false;
+                canMove = true;
+                canFlip = true;
+            }
+        }
+    }
     private void UpdateAnimations()
     {
         anim.SetBool("isWalking", isWalking);
         anim.SetBool("isGrounded", isGrounded);
         anim.SetFloat("yVelocity", rb.velocity.y);
         anim.SetBool("isWallSliding", isWallSliding);
-        anim.SetBool("canClimbLege", canClimbLege);
+
+        
     }
     private void jump()
     {
